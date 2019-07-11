ce4666a1b5b1d3aa5d46c9525bf833d1d96b6942
enzo.tokita <jpx3666@linecorp.com>
2019-07-11 16:45:55 +0900

    first commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by 時田円象 on 2019/07/11.
+//  Copyright © 2019 Enzo Tokita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
